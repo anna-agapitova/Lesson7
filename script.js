d06110fd7cdb9c42d4f5db58a267adf1ce06a081
student <anna.agapitova.baeva@gmail.com>
2019-11-09 11:41:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log(21); // integer
+console.log(3.14) //float
+console.log(typeof 13);
+console.log(typeof 3.14);
+
+console.log('This is BOOTCAMP!!!'); //string
+console.log("This is BOOTCAMP!!!"); //string - no difference/almost
+console.log(`This is BOOTCAMP!!!`); //string - with backtick
+console.log(typeof 'foo');
+
+console.log(true); //bool
+console.log(false); //bool
+console.log(typeof true);
+console.log(typeof false);
+
+console.log(null) // null
+console.log(typeof null)
+
+console.log([1, 2, 3, 4, 5]); // array
+console.log(typeof [1, 2, 3, 4, 5]); //array
+console.log([1, 2, 'foo', 'bar', true, false]);
+
+var someNumber = 123;
+console.log(someNumber);
+someNumber = 321;
+console.log(someNumber);
+var someString = 'Bar';
+console.log(someString);
